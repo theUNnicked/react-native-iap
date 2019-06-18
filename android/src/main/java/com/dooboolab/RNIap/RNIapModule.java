10014cfb19a9ebe9e76965299d8be00e64521c18
Krzysztof Świeczkowski <swieczkowski.krzysztof@gmail.com>
2019-06-18 18:31:35 +0200

    Migrate to AndroidX option from Android Studio

--- android/src/main/java/com/dooboolab/RNIap/RNIapModule.java
+++ android/src/main/java/com/dooboolab/RNIap/RNIapModule.java
@@ -2,7 +2,7 @@
 package com.dooboolab.RNIap;
 
 import android.app.Activity;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.Log;
 
 import com.android.billingclient.api.BillingResult;
@@ -32,7 +32,6 @@ import com.android.billingclient.api.SkuDetails;
 import com.android.billingclient.api.SkuDetailsParams;
 import com.android.billingclient.api.SkuDetailsResponseListener;
 import com.facebook.react.bridge.ReadableArray;
-import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.WritableNativeArray;
